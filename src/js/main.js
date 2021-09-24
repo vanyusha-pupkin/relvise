a26af20c62dea5eb67caf046d3ec00f0118f005e
vany pupkin <vany_pupkin@mail.ru>
2021-09-24 12:03:00 +0600

    add fonts

--- src/js/main.js
+++ src/js/main.js
@@ -8,30 +8,6 @@
 
 // MODULS ===========================================================
 
-// EXAMPLES ===========================================================
-import spoilers from "./modules/_spoilers";
-spoilers()
-import "./examples/_029-movement";
-import "./modules/_dynamic-adaptive";
-import "./modules/_modals";
-import animationBlock from "./modules/_animation-blocks";
-animationBlock()
-import "./examples/_025-star-rating";
-// import "./examples/_23-full-screen-landingpage";
-import "./examples/_026-menu";
-import "./examples/_022-slider-swiper";
-import "./examples/_020-form";
-import tabs from "./modules/_tabs";
-tabs();
-import "./vendor/_lightgallery.min";
-import "./modules/_gallery";
-import "./examples/_105-maps";
-import "./examples/_027-lazy-loading";
-import "./examples/_102-filters";
-import "./examples/_106-hamburgers";
-import actionsForm from "./modules/_forms";
-actionsForm();
-
 
 
 // JS ===========================================================
